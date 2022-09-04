dbbefa955fe9dfada528ce95a66a9d302add538a
Neo Fu <rtugeek@gmail.com>
2022-09-04 12:16:48 +0800

    git init

---
+++ AliSls/DeviceUtil.cs
@@ -0,0 +1,78 @@
+﻿using System.Management;
+using System.Net.NetworkInformation;
+using Microsoft.Win32;
+
+namespace AliSls;
+
+public static class DeviceUtil
+{
+    public static string? GetSystemId()
+    {
+        string systemId = null;
+        using (ManagementObjectSearcher mos =
+               new ManagementObjectSearcher("select * from Win32_ComputerSystemProduct"))
+        {
+            foreach (var item in mos.Get())
+            {
+                systemId = item["UUID"].ToString();
+            }
+        }
+
+        return systemId;
+    }
+
+    public static string? GetMac()
+    {
+        var macAddr =
+        (
+            from nic in NetworkInterface.GetAllNetworkInterfaces()
+            where nic.OperationalStatus == OperationalStatus.Up
+            select nic.GetPhysicalAddress().ToString()
+        ).FirstOrDefault();
+        return macAddr;
+    }
+
+    public static String? GetMachineGuid()
+    {
+        try
+        {
+            string x64Result = string.Empty;
+            string x86Result = string.Empty;
+            //此处如果需要可以判断一下系统是什么位数的,比如32或者64,用以取分读取哪个视图,因为视图不同,获得的数据也不同
+            RegistryKey keyBaseX64 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            RegistryKey keyBaseX86 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+            RegistryKey keyX64 = keyBaseX64.OpenSubKey(@"SOFTWARE\Microsoft\Cryptography",
+                RegistryKeyPermissionCheck.ReadSubTree);
+            RegistryKey keyX86 = keyBaseX86.OpenSubKey(@"SOFTWARE\Microsoft\Cryptography",
+                RegistryKeyPermissionCheck.ReadSubTree);
+            object resultObjX64 = keyX64.GetValue("MachineGuid", (object)"default");
+            object resultObjX86 = keyX86.GetValue("MachineGuid", (object)"default");
+            keyX64.Close();
+            keyX86.Close();
+            keyBaseX64.Close();
+            keyBaseX86.Close();
+            keyX64.Dispose();
+            keyX86.Dispose();
+            keyBaseX64.Dispose();
+            keyBaseX86.Dispose();
+            keyX64 = null;
+            keyX86 = null;
+            keyBaseX64 = null;
+            keyBaseX86 = null;
+            if (resultObjX64 != null && resultObjX64.ToString() != "default")
+            {
+                return resultObjX64.ToString();
+            }
+
+            if (resultObjX86 != null && resultObjX86.ToString() != "default")
+            {
+                return resultObjX86.ToString();
+            }
+        }
+        catch (Exception)
+        {
+        }
+
+        return null;
+    }
+}
---
+++ AliSls/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.NetworkInformation;
+using Aliyun.Api.LogService;
+using Aliyun.Api.LogService.Domain.Log;
+using Aliyun.Api.LogService.Infrastructure.Protocol.Http;
+using CommandLine;
+
+namespace AliSls
+{
+    static class Program
+    {
+        static async Task Main(String[] args)
+        {
+            var SmBIOS = DeviceUtil.GetSystemId();
+            var mac = DeviceUtil.GetMac();
+            var machineGUID = DeviceUtil.GetMachineGuid();
+            Console.WriteLine(machineGUID);
+            ParserResult<Options> parserResult = Parser.Default.ParseArguments<Options>(args);
+            var options = parserResult.Value!;
+            // 1. 构建 `ILogServiceClient` 实例：
+            var client = LogServiceClientBuilders.HttpBuilder
+                .Endpoint(options.Endpoint, options.Project) // 设置「服务入口」和「项目名称」。
+                .Credential(options.AccessKeyId, options.AccessKey) // 设置「访问密钥」（RAM模式）
+                .Build();
+
+            // 2. 使用 `client` **异步**访问接口（请注意 `await`）：
+            LogGroupInfo groupInfo = new LogGroupInfo();
+            groupInfo.Topic = options.Topic;
+            var logInfo = new LogInfo();
+            logInfo.Time = DateTimeOffset.Now;
+            logInfo.Contents.Add("content", options.Content);
+            logInfo.Contents.Add("platform", options.Platform);
+            logInfo.Contents.Add("MAC", mac);
+            logInfo.Contents.Add("machineGUID", machineGUID);
+            logInfo.Contents.Add("SmBIOS", SmBIOS);
+            logInfo.Contents.Add("version", options.AppVersion);
+            logInfo.Contents.Add("app", options.AppName);
+            groupInfo.Logs.Add(logInfo);
+            var response = await client.PostLogStoreLogsAsync(options.Logstore, groupInfo);
+
+            // 3. 从 `response` 中获取业务结果：
+            var result = response
+                .EnsureSuccess(); // 此方法会确保返回的响应失败时候抛出 `LogServiceException`。
+            Console.WriteLine(result.IsSuccess);
+            var resultStr = await result.GetHttpResponseMessage().Content.ReadAsStringAsync();
+            Console.WriteLine(resultStr);
+        }
+    }
+
+    public class Options
+    {
+        [Option('p', "project", Required = false, Default = "haihaihai", HelpText = "Project name")]
+        public string Project { get; set; }
+
+        [Option('l', "logstore", Required = true, HelpText = "Log store name")]
+        public string Logstore { get; set; }
+
+        [Option('i', "key-id", Required = true, HelpText = "accessKeyId")]
+        public string AccessKeyId { get; set; }
+
+        [Option('k', "key", Required = true, HelpText = "accessKeyId")]
+        public string AccessKey { get; set; }
+
+        [Option('c', "content", Required = false, Default = "", HelpText = "Content")]
+        public string Content { get; set; }
+
+        [Option('p', "platform", Required = false, Default = "windows", HelpText = "Platforms: windows macOS etc.")]
+        public string Platform { get; set; }
+
+        [Option('v', "app-version", Required = true, HelpText = "Application version")]
+        public string AppVersion { get; set; }
+
+        [Option('n', "app-name", Required = true, HelpText = "Application name")]
+        public string AppName { get; set; }
+
+        [Option('e', "endpoint", Required = false, Default = "cn-guangzhou.log.aliyuncs.com",
+            HelpText = "Application version")]
+        public string Endpoint { get; set; }
+
+        [Option('t', "topic", Required = false, Default = "", HelpText = "Topic of this log")]
+        public string Topic { get; set; }
+    }
+}
